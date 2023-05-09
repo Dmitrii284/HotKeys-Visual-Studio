8e82a184b912c584fb3f24044356e2ef9af3ee13
Дмитрий Феофанов <125288280+Dmitrii284@users.noreply.github.com>
2023-05-10 04:43:08 +0500

    5

--- HotKeys-VisualStudio/Main.cpp
+++ HotKeys-VisualStudio/Main.cpp
@@ -1,9 +1,16 @@
-#include<iostream>
+﻿#include<iostream>
 #include<direct.h>
+#include<sstream>
+#include<fstream>
 
 
 int main() {
 	setlocale(LC_ALL, "Rus");
-	_mkdir("c:/myfolder");
+		
+	std::cout << "Введите путь для создания папки -> ";
+	std::string fold_path;
+	std::getline(std::cin, fold_path);
+	_mkdir("fold_path");
+
 	return 0;	
 }
