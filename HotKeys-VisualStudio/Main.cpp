6edcb62645e2c356a84108ddf131f51f9c996049
Дмитрий Феофанов <125288280+Dmitrii284@users.noreply.github.com>
2023-05-14 02:37:40 +0500

    22

--- HotKeys-VisualStudio/Main.cpp
+++ HotKeys-VisualStudio/Main.cpp
@@ -10,7 +10,7 @@ int main() {
 	std::cout << " Создать папку: ";
 	std::cin >> n;
 	char *dirname = &n;
-	check = _mkdir(dirname);
+	check = _mkdir(dirname,0777);
 	// check if directory is created or not
 	if (!check)
 		printf("Вы создали папку\n");
